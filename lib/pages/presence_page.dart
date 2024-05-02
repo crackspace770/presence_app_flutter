c96dd9c97d1c591321074e46c460e31c555132d0
fajar <crackspace770>
2024-05-02 12:37:20 +0700

    upload presence to firestore

--- lib/pages/presence_page.dart
+++ lib/pages/presence_page.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:presence_app/service/firestore.dart';
 
 class PresencePage extends StatefulWidget {
   const PresencePage({Key? key}) : super(key: key);
@@ -11,130 +19,282 @@ class _PresencePageState extends State<PresencePage> {
   String? selectedStatus;
   String hintText = 'Status';
 
+  final user = FirebaseAuth.instance.currentUser;
+  final FirestoreService firestoreService = FirestoreService();
+
+  //controller
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController keteranganController = TextEditingController();
+
+  File? imageFile; // Variable to store the captured image
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  // Function to fetch user data from Firestore
+
+  void fetchUserData() async {
+    try {
+      // Fetch user document from Firestore based on user's UID
+      QuerySnapshot userDocs = await FirebaseFirestore.instance.collection('users').where('uid', isEqualTo: user?.uid).get();
+
+      if (userDocs.docs.isNotEmpty) {
+        // Extract first name and ID from user document
+        Map<String, dynamic>? userData = userDocs.docs.first.data() as Map<String, dynamic>?;
+
+        // Set first name and ID to controllers
+        nameController.text = userData?['first_name'] ?? '';
+        idController.text = userData?['id_pegawai'] ?? '';
+      } else {
+        print('User document not found');
+        // Handle case when user document is not found
+      }
+    } catch (error) {
+      print('Error fetching user data: $error');
+      // Handle error fetching user data
+    }
+  }
+
+  // Function to open camera and capture image
+  Future<void> _openCamera() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Presence"),
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 25.0),
-              child: Text("Ambil Foto selfie"),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                width: 400,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blue),
-                  shape: BoxShape.rectangle,
-                ),
-                child: Icon(
-                  Icons.camera_alt,
-                  size: 150,
-                  color: Colors.blue,
-                ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: Text("Ambil Foto selfie"),
               ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26),
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Nama Pegawai", border: InputBorder.none),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTap: _openCamera, // Open camera when icon is tapped
+                  child: Container(
+                    width: 400,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: imageFile != null // Show captured image if available
+                        ? Image.file(
+                      imageFile!,
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    )
+                        : Icon(
+                      Icons.camera_alt,
+                      size: 150,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26),
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "ID Pegawai", border: InputBorder.none),
+
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26),
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: TextField(
+                      controller: nameController,
+                      enabled: false,
+                      decoration: InputDecoration(
+                          hintText: "Nama Pegawai", border: InputBorder.none),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26),
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: DropdownButton<String>(
-                    hint: Text(hintText),
-                    value: selectedStatus,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedStatus = newValue;
-                        hintText = newValue ?? 'Status';
-                      });
-                    },
-                    items: <String>['Check In', 'Check Out', 'Izin']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26),
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: TextField(
+                      controller: idController,
+                      enabled: false,
+                      decoration: InputDecoration(
+                          hintText: "ID Pegawai", border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26),
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: DropdownButton<String>(
+                      hint: Text(hintText),
+                      value: selectedStatus,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedStatus = newValue;
+                          hintText = newValue ?? 'Status';
+                        });
+                      },
+                      items: <String>['Check In', 'Check Out', 'Izin']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Text(
+                  "Presence Status: ${selectedStatus ?? 'Select a status'}",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              SizedBox(height: 15),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26),
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: TextField(
+                      controller: keteranganController,
+                      decoration: InputDecoration(
+                          hintText: "Keterangan", border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 25),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: GestureDetector(
+                  onTap: () async {
+                    if (imageFile != null && selectedStatus != null) {
+
+                      List<int> imageBytes = imageFile!.readAsBytesSync();
+                      String photo = base64Encode(imageBytes);
+                      bool status = selectedStatus == 'Check In'; // Example logic for status
+
+                      try {
+                        await firestoreService.addPresence(
+
+                          imageFile: imageFile!, // Pass the imageFile
+                          status: status,
+                          id: idController.text,
+                          info: keteranganController.text,
+                          name: nameController.text,
+                        );
+
+                        await firestoreService.addUserPresence(
+                          imageFile: imageFile!, // Pass the imageFile
+                          status: status,
+                          id: idController.text,
+                          info: keteranganController.text,
+                          name: nameController.text,
+                        );
+
+                        // Optionally, show a success message or navigate to another screen
+                      } catch (error) {
+                        // Handle error
+                        print('Error adding presence: $error');
+                        // Show error message to user
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Error adding presence. Please try again.'),
+                          ),
+                        );
+                      }
+                    } else {
+                      // Show error message if image or status is not selected
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please capture photo and select status'),
+                        ),
                       );
-                    }).toList(),
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Absen!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Text(
-                "Presence Status: ${selectedStatus ?? 'Select a status'}",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
 
-            SizedBox(height: 15),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black26),
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Keterangan", border: InputBorder.none),
-                  ),
-                ),
-              ),
-            ),
-
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+_onCameraView () async {
+
+  final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+  final isIos = defaultTargetPlatform == TargetPlatform.iOS;
+  final isNotMobile = !(isAndroid || isIos);
+  if(isNotMobile) return;
+
+}
+
+
+
--- lib/service/firestore.dart
+++ lib/service/firestore.dart
@@ -1,6 +1,9 @@
 
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import '../model/user.dart';
 
@@ -10,44 +13,109 @@ class FirestoreService{
   late CollectionReference presences;
   late CollectionReference userPresences;
 
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> _uploadImageToStorage(File imageFile, String userId) async {
+    try {
+      // Create a reference to the location you want to upload to in Firebase Storage
+      Reference storageReference = _storage.ref().child('photo/selfie/$userId');
+
+      // Upload the file to Firebase Storage
+      UploadTask uploadTask = storageReference.putFile(imageFile);
+
+      // Wait for the upload to complete
+      await uploadTask;
+
+      // Get the download URL of the uploaded file
+      String downloadURL = await storageReference.getDownloadURL();
+
+      return downloadURL;
+    } catch (e) {
+      print('Error uploading image to Firebase Storage: $e');
+      throw e;
+    }
+  }
+
   FirestoreService() {
-    uid = FirebaseAuth.instance.currentUser?.uid;
     presences = FirebaseFirestore.instance.collection('presence');
-    userPresences = FirebaseFirestore.instance.collection('user').doc(uid).collection('presence');
+    init();
+  }
+
+  Future<void> init() async {
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      QuerySnapshot userDocs = await FirebaseFirestore.instance.collection('users').where('uid', isEqualTo: uid).get();
+      if (userDocs.docs.isNotEmpty) {
+        DocumentSnapshot userDoc = userDocs.docs.first;
+        userPresences = userDoc.reference.collection('presence');
+      } else {
+        // Handle case when user document is not found
+      }
+    } else {
+      // Handle the case where the user is not authenticated
+    }
   }
 
 
-  Future<void> addPresence(
-      String presence, String photo, bool status, String id, String info, String name ) {
+  Future<void> addPresence({
+    required File imageFile,
+    required bool status,
+    required String id,
+    required String info,
+    required String name,
+  }) async {
+    try {
+      String userId = FirebaseAuth.instance.currentUser!.uid;
 
-    return presences.add( {
-      'id': id,
-      'name' : name,
-      'info': info,
-      'presence' : presence,
-      'photo': photo,
-      'status': status,
-      'timestamp' : Timestamp.now(),
-      'date' : DateTime.now()
-    });
+      // Upload the image to Firebase Storage
+      String photoURL = await _uploadImageToStorage(imageFile, userId);
 
+      // Add presence data to Firestore with the photo URL
+      await presences.add({
+        'id': id,
+        'name': name,
+        'info': info,
+        'photo': photoURL,
+        'status': status,
+        'timestamp': Timestamp.now(),
+        'date': DateTime.now(),
+      });
+    } catch (error) {
+      print('Error adding presence: $error');
+      throw error;
+    }
   }
 
-  Future<void> addUserPresence(
-      String presence, String photo, bool status, String id, String info, String name ) {
+  Future<void> addUserPresence({
+    required File imageFile,
+    required bool status,
+    required String id,
+    required String info,
+    required String name,
+  }) async {
+    try {
+      String userId = FirebaseAuth.instance.currentUser!.uid;
 
-    return userPresences.add( {
-      'id': id,
-      'name' : name,
-      'info': info,
-      'presence' : presence,
-      'photo': photo,
-      'status': status,
-      'timestamp' : Timestamp.now(),
-      'date' : DateTime.now()
-    });
+      // Upload the image to Firebase Storage
+      String photoURL = await _uploadImageToStorage(imageFile, userId);
 
+      // Add presence data to Firestore with the photo URL
+      await userPresences.add({
+        'id': id,
+        'name': name,
+        'info': info,
+        'photo': photoURL,
+        'status': status,
+        'timestamp': Timestamp.now(),
+        'date': DateTime.now(),
+      });
+    } catch (error) {
+      print('Error adding presence: $error');
+      throw error;
+    }
   }
+
+
 
   Stream<QuerySnapshot> getPresences() {
     final presenceStream = presences.orderBy('timestamp', descending:true).snapshots();
